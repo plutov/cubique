02ea300bcf4600e52be17edcca9948336efbf1f9
Alexander Plutov <alexander.plutov@gmail.com>
2011-10-14 10:46:31 +0300

    INSTALL, LICENSE moved to Wiki. Added VERSION

--- src/library/Cubique/Exception.php
+++ src/library/Cubique/Exception.php
@@ -3,7 +3,7 @@
  * Cubique_Exception
  * @author    Alexander Plutov
  * @copyright (c) 2011 Alexander Plutov
- * @license   https://github.com/plutov/cubique/blob/master/LICENSE
+ * @license   https://github.com/plutov/cubique/wiki/License
  */
 class Cubique_Exception extends Exception
 {
--- src/public/helpers/js/cubique.js
+++ src/public/helpers/js/cubique.js
@@ -2,7 +2,7 @@
  * Cubique
  * @author    Alexander Plutov
  * @copyright (c) 2011 Alexander Plutov
- * @license   https://github.com/plutov/cubique/blob/master/LICENSE
+ * @license   https://github.com/plutov/cubique/wiki/License
  */
 
 /**
