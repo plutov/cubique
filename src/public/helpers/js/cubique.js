770a16c16d1b72ec91aa68174fcee7c108d8d8ab
Alexander Plutov <alexander.plutov@gmail.com>
2011-12-16 17:50:52 +0300

    Added @link parameter to doc comments.

--- src/public/helpers/js/cubique.js
+++ src/public/helpers/js/cubique.js
@@ -2,6 +2,7 @@
  * Cubique
  * @author    Alexander Plutov
  * @copyright (c) 2011 Alexander Plutov
+ * @link      https://github.com/plutov/cubique
  * @license   https://github.com/plutov/cubique/wiki/License
  */
 
