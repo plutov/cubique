d17c5d143dece459bb957edef3b2eb86f76e1946
Alexander Plutov <alexander.plutov@gmail.com>
2011-12-16 12:02:54 +0300

    Added controller and view files with default configuration.

---
+++ tests/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+class IndexController extends Zend_Controller_Action
+{
+    public function indexAction()
+    {
+        $grid = new Cubique_Grid('province');
+        $grid->setTable('province')
+                ->setColumns(array(
+                    'name'        => 'Name',
+                    'code'        => 'Code',
+                    'country'     => 'Country'
+                ))
+                ->setDefaultOrder('name')
+                ->setColumnsToSort(array('name', 'code', 'country'))
+                ->setColumnsToEscape(array('name', 'code'))
+                ->setColumnsToSearch(array('name', 'code', 'country'))
+                ->setJoin('country', 'country', 'country_id', 'id', 'name');
+        $grid->dispatch();
+        $this->view->grid = $grid;
+    }
+}
