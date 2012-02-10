8768ddb6f8f2ae4547c7e416ca9672e0c1ff0195
Alexander Plutov <a@plutov.by>
2012-02-10 13:24:42 +0300

    Fixed file docs.

--- src/library/Cubique/Exception.php
+++ src/library/Cubique/Exception.php
@@ -2,7 +2,7 @@
 /**
  * Cubique_Exception
  * @author    Alexander Plutov
- * @copyright (c) 2011 Alexander Plutov
+ * @copyright (c) 2011-2012 Alexander Plutov
  * @license   https://github.com/plutov/cubique/wiki/License
  */
 class Cubique_Exception extends Exception
--- src/public/helpers/css/cubique.css
+++ src/public/helpers/css/cubique.css
@@ -1,7 +1,7 @@
 /**
  * Cubique
  * @author    Alexander Plutov
- * @copyright (c) 2011 Alexander Plutov
+ * @copyright (c) 2011-2012 Alexander Plutov
  * @link      https://github.com/plutov/cubique
  * @license   https://github.com/plutov/cubique/wiki/License
  */
