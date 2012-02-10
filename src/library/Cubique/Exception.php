2b7d6770ad96cb6ba2be3b83f017d8a8be55b1b8
Alexander Plutov <a@plutov.by>
2012-02-10 13:37:05 +0300

    Added link parameter to docs.

--- src/library/Cubique/Exception.php
+++ src/library/Cubique/Exception.php
@@ -3,6 +3,7 @@
  * Cubique_Exception
  * @author    Alexander Plutov
  * @copyright (c) 2011-2012 Alexander Plutov
+ * @link      https://github.com/plutov/cubique
  * @license   https://github.com/plutov/cubique/wiki/License
  */
 class Cubique_Exception extends Exception
