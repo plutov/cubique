054c40acb3f38b1014fd596b6b70c97039364841
Alexander Plutov <alexander.plutov@gmail.com>
2012-02-14 00:06:33 +0300

    Changed test SQL, view and controller.

--- tests/IndexController.php
+++ tests/IndexController.php
@@ -5,17 +5,24 @@ class IndexController extends Zend_Controller_Action
     {
         $grid = new Cubique_Grid('province');
         $grid->setTable('province')
-            ->setColumns(array(
-                'name'        => 'Name',
-                'code'        => 'Code',
-                'country'     => 'Country'
-            ))
-            ->setSpecialData('name', '<b><!name!></b>')
-            ->setDefaultOrder('name')
-            ->setColumnsToSort(array('name', 'code', 'country'))
-            ->setColumnsToEscape(array('code'))
-            ->setColumnsToSearch(array('name', 'code', 'country'))
-            ->setJoin('country', 'country', 'country_id', 'id', 'name');
+             ->setColumns(array(
+                 'name'        => 'Name',
+                 'code'        => 'Code',
+                 'country'     => 'Country'
+             ))
+             //->setSpecialDataSeparators('<<!!', '!!>>')
+             //->setSpecialData('name', '<b><<!!name!!>></b>')
+             ->setDefaultOrder('name')
+             ->setColumnsToSort(array('name', 'code', 'country'))
+             //->setColumnsToEscape(array('code'))
+             ->setColumnsToSearch(array('name', 'code', 'country'))
+             //->setRowsOnPage(15)
+             //->logErrors(APPLICATION_PATH . '/logs/cubique')
+             //->setUrl('/')
+             //->setErrorMessage('Error!')
+             //->addWhere('country.name LIKE "A%"')
+             //->addWhere('province.code LIKE "A%"', false)
+             ->setJoin('country', 'country', 'country_id', 'id', 'name');
         $grid->dispatch();
         $this->view->grid = $grid;
     }
