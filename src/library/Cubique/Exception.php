6c107a595c64bd01b6e894b5cab2d6046d6eff3d
Alexander Plutov <a@plutov.by>
2012-03-05 19:15:08 +0300

    Link to a file instead of wiki page.

--- src/library/Cubique/Exception.php
+++ src/library/Cubique/Exception.php
@@ -4,7 +4,7 @@
  * @author    Alexander Plutov
  * @copyright (c) 2011-2012 Alexander Plutov
  * @link      https://github.com/plutov/cubique
- * @license   https://github.com/plutov/cubique/wiki/License
+ * @license   https://github.com/plutov/cubique/blob/master/LICENSE
  */
 class Cubique_Exception extends Exception
 {
--- src/public/helpers/js/cubique.js
+++ src/public/helpers/js/cubique.js
@@ -3,7 +3,7 @@
  * @author    Alexander Plutov
  * @copyright (c) 2011-2012 Alexander Plutov
  * @link      https://github.com/plutov/cubique
- * @license   https://github.com/plutov/cubique/wiki/License
+ * @license   https://github.com/plutov/cubique/blob/master/LICENSE
  */
 
 /**
